9af50f8c1769721ea9c23ee97ef3526be163980c
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2023-05-02 17:12:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+import cv2
+import pandas as pd
+import numpy as np
+from ultralytics import YOLO
+from tracker import*
+import cvzone
+
+model=YOLO('yolov8s.pt')
+
+
+
+def RGB(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE :  
+        colorsBGR = [x, y]
+        print(colorsBGR)
+        
+
+cv2.namedWindow('RGB')
+cv2.setMouseCallback('RGB', RGB)
+
+cap=cv2.VideoCapture('p3.mp4')
+
+
+my_file = open("coco.txt", "r")
+data = my_file.read()
+class_list = data.split("\n") 
+#print(class_list)
+
+count=0
+
+tracker=Tracker()
+
+
+while True:    
+    ret,frame = cap.read()
+    if not ret:
+        break
+    count += 1
+    if count % 3 != 0:
+        continue
+    frame=cv2.resize(frame,(1020,500))
+   
+
+    results=model.predict(frame)
+ #   print(results)
+    a=results[0].boxes.boxes
+    px=pd.DataFrame(a).astype("float")
+#    print(px)
+    list=[]         
+    for index,row in px.iterrows():
+#        print(row)
+ 
+        x1=int(row[0])
+        y1=int(row[1])
+        x2=int(row[2])
+        y2=int(row[3])
+        d=int(row[5])
+        c=class_list[d]
+        if 'person' in c:
+
+            cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),1)
+            cv2.putText(frame,str(c),(x1,y1),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),1)
+
+    cv2.imshow("RGB", frame)
+    if cv2.waitKey(1)&0xFF==27:
+        break
+cap.release()
+cv2.destroyAllWindows()
+
